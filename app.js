676e20a1032322708f8aec7d02eaaa59278aa90f
Nardosha <lalabumlala@gmail.com>
2022-05-30 19:49:30 +0300

    refactor: rename selectors according to rule 3.3 in index.html, style.css, app.js

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new");//Add a new task.
 var addButton=document.querySelector(".add-block__button");//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("todo");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("done");//completed-tasks
 
 
 //New task list item
